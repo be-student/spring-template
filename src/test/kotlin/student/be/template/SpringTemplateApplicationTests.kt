a8ca19d302f60d62032fb98b6ee4e214604dbf48
eunwoo song <presentsong@naver.com>
2024-08-24 00:19:49 +0900

    feat: initial commit

---
+++ src/test/kotlin/student/be/template/SpringTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package student.be.template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
